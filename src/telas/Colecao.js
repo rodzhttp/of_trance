847180cb3b623feb0c1757189ddc416be8d365b9
Rodrigo Sousa <rodlinnk@gmail.com>
2022-04-10 23:51:15 -0300

    mexendo nos componentes

--- src/telas/Colecao.js
+++ src/telas/Colecao.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { StyleSheet, Image, Dimensions, View } from 'react-native';
 import topo from '../../assets/topo2.png';
 import prod1 from '../../assets/prod1.png';
+import prod2 from '../../assets/prod2.png';
 import Texto from 'C:/Users/Rodrigo/Desktop/OFTRANCE/app-of-trance/src/componentes/Texto.js';
 
 //constante pra receber o tamanho da imagem 
@@ -23,34 +24,55 @@ export default function Colecao(){
 
         <View style={estilos.colecao}>
             <Image source={prod1} style={estilos.imgproduto}/>
-            <Texto style={estilos.prod1}>Macacão curto com manga flare</Texto>         
+            <View style={estilos.colecao2}>
+                <Texto style={estilos.produto}>Macacão curto com manga flare</Texto> 
+                <Texto style={estilos.preco}>R$100,00</Texto>    
+            </View>
         </View>
-        <Texto style={estilos.preco1}>R$100,00</Texto>
-        
+        <View style={estilos.colecao}>
+            <Image source={prod2} style={estilos.imgproduto}/>
+            <View style={estilos.colecao2}>
+                <Texto style={estilos.produto}>Macacão curto com manga flare</Texto> 
+                <Texto style={estilos.preco}>R$100,00</Texto>    
+            </View>
+        </View>
+        <View style={estilos.colecao}>
+            <Image source={prod2} style={estilos.imgproduto}/>
+            <View style={estilos.colecao2}>
+                <Texto style={estilos.produto}>Macacão curto com manga flare</Texto> 
+                <Texto style={estilos.preco}>R$100,00</Texto>    
+            </View>
+        </View>
+
 
     </>
 }
 
 const estilos = StyleSheet.create({
-    preco1:{ //preço do produto 1
+    preco:{ //preço do produto 1
         color: '#4B0082',
         fontWeight: 'bold',
         fontSize: 25,
-        lineHeight: 50,
-        marginHorizontal: 20
+        // lineHeight: 50,
+        // marginHorizontal: 20
 
     },
-    prod1: { //produto 1
+    produto: {
         color: '#4B0082',
         fontWeight: 'bold',
-        lineHeight: 80,
+        lineHeight: 20,
+        marginTop: 45,
 
     },
     imgproduto: { //imagem do produto
         width: 80,
         height: 80,
         marginHorizontal: 20,
-        marginVertical: 20
+        marginVertical: 20,
+        borderRadius: 15,
+        
+    },
+    colecao2: {
 
     },
     colecao: { //
